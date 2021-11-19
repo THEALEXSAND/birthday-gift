b64fc1110d9ff1533510b63b26dd13c383d9f1b7
Alexander Sandoval Torrealba <gohangaymer878@gmail.com>
2021-11-19 01:27:29 -0400

    First Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const card = document.getElementById('card');
+const cover = document.querySelector('.card__cover');
+const back = document.querySelector('.card__back');
+
+card.addEventListener('click', () => {
+    card.classList.toggle('turn');
+    Hide(cover);
+    
+    setTimeout(() => {
+        card.style.boxShadow = '-5px 5px 2px hsl(322, 100%, 40%)'
+        cover.classList.toggle('hide__display');
+    }, 500);
+    
+    Hide(back);
+})
+
+function Hide(element) {
+    element.classList.toggle('hide');
+}
+
+const date = new Date();
+const msg = document.querySelector('.msg');
+
+if (date.getDate() == 19) {
+    msg.textContent = 'Te agradezco por ser mi amiga y no haberte alejado a pesar de ciertos problemas. Por ello te hago este pequeño regalo y aunque no sea gran cosa espero te haya gustado; ya dejando lo cursi, disfruta tu día especial y pasala bien.'
+}
